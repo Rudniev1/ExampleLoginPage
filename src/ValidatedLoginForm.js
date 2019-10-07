6c63d0bd8c60b33a32404f0ab38cce64234d7546
Rudniev1 <shqdnix1112@gmail.com>
2019-10-07 17:16:35 +0200

    Deleting LoginForm.js

--- src/ValidatedLoginForm.js
+++ src/ValidatedLoginForm.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Formik } from "formik";
-import EmailValidator from "email-validator";
-import Yup from "yup";
-import LoginForm from "./LoginForm";
+import * as EmailValidator from "email-validator";
+import * as Yup from "yup";
 
 const ValidatedLoginForm = () => (
   <Formik
@@ -13,8 +12,60 @@ const ValidatedLoginForm = () => (
         setSubmitting(false);
       }, 500);
     }}
+    validationSchema={Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required("Required"),
+      password: Yup.string()
+        .required("No password provided.")
+        .min(8, "Password is too short - should be 8 chars minimum.")
+        .matches(/(?=.*[0-9])/, "Password must contain a number.")
+    })}
   >
-    <h1>Validated Login Form</h1>
+    {props => {
+      const {
+        values,
+        touched,
+        errors,
+        isSubmitting,
+        handleChange,
+        handleBlur,
+        handleSubmit
+      } = props;
+      return (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="text"
+            placeholder="Enter your email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={errors.email && touched.email && "error"}
+          />
+          {errors.email && touched.email && (
+            <div className="input-feedback">{errors.email}</div>
+          )}
+          <label htmlFor="email">Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={errors.password && touched.password && "error"}
+          />
+          {errors.password && touched.password && (
+            <div className="input-feedback">{errors.password}</div>
+          )}
+          <button type="submit" disabled={isSubmitting}>
+            Login
+          </button>
+        </form>
+      );
+    }}
   </Formik>
 );
 
